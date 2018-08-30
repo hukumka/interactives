f4aac5aab0af331233ad86d4c154d27bcc6c54dc
Hukumka <hukumka212@gmail.com>
2018-08-30 15:47:36 +0000

    Implemented virtual machine functionality

--- crates/vm/src/lib.rs
+++ crates/vm/src/lib.rs
@@ -3,13 +3,20 @@
 //!
 //!
 //! 
-#![feature(rust_2018_preview, uniform_paths)]
+#![feature(
+    rust_2018_preview, 
+    uniform_paths,
+    vec_resize_default
+)]
 
 mod types;
 
+use std::collections::HashSet;
+
 use types::{
     ArbitraryData,
-    ArbitraryDataEnum,   
+    LocalDataPointer,
+    DataStack,
 };
 
 
@@ -42,61 +49,93 @@ pub enum Instruction{
     // ---- ----
     // Comparison: Int
     /// calculate binary operator ==
-    Int_Eq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    IntEq(Binary),
 
     /// calculate binary operator !=
-    Int_NotEq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    IntNotEq(Binary),
 
     /// calculate binary operator <
-    Int_Less(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    IntLess(Binary),
 
     /// calculate binary operator <=
-    Int_LessOrEq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    IntLessOrEq(Binary),
 
     // ---- ----
     // Comparison: Float
     /// calculate binary operator ==
-    Float_Eq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    FloatEq(Binary),
 
     /// calculate binary operator !=
-    FLoat_NotEq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    FloatNotEq(Binary),
 
     /// calculate binary operator <
-    Float_Less(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    FloatLess(Binary),
 
     /// calculate binary operator <=
-    FLoat_LessOrEq(Binary),
+    ///
+    /// return value stored as int
+    /// + 0 if false
+    /// - 1 if true
+    FloatLessOrEq(Binary),
 
     // ---- ----
     // Arithmetic: Int
     /// Calculate binary operator +
-    Int_Sum(Binary),
+    IntSum(Binary),
 
     /// Calculate binary operator -
-    Int_Diff(Binary),
+    IntDiff(Binary),
 
     /// Calculate binary operator *
-    Int_Mul(Binary),
+    IntMul(Binary),
 
     /// Calculate binary operator /
-    Int_Div(Binary),
+    IntDiv(Binary),
 
     /// Calculate binary operator %
-    Int_Mod(Binary),
+    IntMod(Binary),
 
     // ---- ----
     // Arithmetic: FLoat
     /// Calculate binary operator +
-    Float_Sum(Binary),
+    FloatSum(Binary),
 
     /// Calculate binary operator -
-    Float_Diff(Binary),
+    FloatDiff(Binary),
 
     /// Calculate binary operator *
-    Float_Mul(Binary),
+    FloatMul(Binary),
 
     /// Calculate binary operator /
-    Float_Div(Binary),
+    FloatDiv(Binary),
 
     // ---- ----
     // Pointers
@@ -123,11 +162,11 @@ pub enum Instruction{
     // ---- ----
     // Constants
     /// Put value into local variable
-    Int_Const(LocalDataPointer, i32),
+    IntConst(LocalDataPointer, i32),
 
     // ---- ----
     /// Put value into local variable
-    Float_Const(LocalDataPointer, i32),
+    FloatConst(LocalDataPointer, f32),
 
     // ---- ----
     // Type convertions
@@ -139,13 +178,11 @@ pub enum Instruction{
 
     // ---- ----
     // Other
-    /// Increment value at address
-    /// stored in local variable
+    /// Increment local variable
     /// (int and alike only)
     Inc(LocalDataPointer),
 
-    /// Decrement value at address
-    /// stored in local variable
+    /// Decrement local variable
     /// (int and alike only)
     Dec(LocalDataPointer),
 
@@ -196,18 +233,324 @@ pub struct InstructionCopy{
 /// General binary operator instruction representation
 pub struct Binary{
     /// result of instruction will be put here
-    pub put_into: CodePointer,
+    pub put_into: LocalDataPointer,
     /// left operand
-    pub left: CodePointer,
+    pub left: LocalDataPointer,
     /// right operand
-    pub right: CodePointer
+    pub right: LocalDataPointer
 }
 
+
 /// references position in code
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub struct CodePointer(usize);
 
-/// references local variable or temporary value
-pub struct LocalDataPointer(usize);
+
+pub struct VirtualMachine<Data>{
+    /// compiled code storage
+    code: Vec<Instruction>,
+    /// current position in code
+    instruction_pointer: CodePointer,
+
+    /// local variable and temporary values storage
+    data_stack: DataStack<Data>,
+    
+    /// map from function id to function offset in ```code```
+    functions: Vec<CodePointer>,
+
+    /// stack containing return points generated after ```Instruction::Call``
+    /// to go back with ```Instruction::Return```
+    call_stack: Vec<CodePointer>,
+
+    /// points in code at there execution pauses
+    breakpoints: HashSet<CodePointer>,
+}
+
+impl<Data: ArbitraryData> VirtualMachine<Data>{
+    /// instantiates virtual machine
+    ///
+    /// Arguments:
+    /// 1. code: Vec<Instruction> - provided by compiler or via deserialization
+    /// 2. functions: Vec<CodePointer> - mapping from function id to function entrance
+    pub fn new(code: Vec<Instruction>, functions: Vec<CodePointer>)->Self{
+        Self{
+            code,
+            functions,
+            instruction_pointer: CodePointer(0),
+
+            data_stack: DataStack::new(),
+
+            call_stack: vec![],
+
+            breakpoints: HashSet::new(),
+        }
+    }
+
+    /// Start/continue execution of program
+    ///
+    /// If program returned value exit with Some(value)
+    ///
+    /// If program reached breakpoint exit with None
+    pub fn run(&mut self)->Result<Option<Data>, Data::Error>{
+        let res = loop{
+            if let Some(return_value) = self.execute_instruction()?{
+                break Some(return_value);
+            }else if self.breakpoints.contains(&self.instruction_pointer){
+                break None;
+            }
+        };
+        Ok(res)
+    }
+
+    /// Execute single instruction on virtual machine
+    ///
+    /// If it result it stoping execution return Some(value)
+    ///
+    /// otherwise returns None
+    fn execute_instruction(&mut self)->Result<Option<Data>, Data::Error>{
+        let instruction = &self.code[self.instruction_pointer.0];
+        self.instruction_pointer.0 += 1;
+        match instruction{
+            // ---- ----
+            // Jumps
+            Instruction::Jump(to) => {
+                self.instruction_pointer = to.clone();
+                Ok(None)
+            },
+            Instruction::JumpZ(to, condition) => {
+                let condition = self.data_stack[condition].as_int()?;
+                if condition == 0{
+                    self.instruction_pointer = to.clone();
+                }
+                Ok(None)
+            },
+            Instruction::JumpNZ(to, condition) => {
+                let condition = self.data_stack[condition].as_int()?;
+                if condition != 0{
+                    self.instruction_pointer = to.clone();
+                }
+                Ok(None)
+            },
+            // ---- ----
+            // Int comparison
+            Instruction::IntEq(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? == b.as_int()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::IntNotEq(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? != b.as_int()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::IntLess(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? < b.as_int()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::IntLessOrEq(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? <= b.as_int()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            // ---- ----
+            // Float comparison
+            Instruction::FloatEq(bin) => {
+                // Warning: comparing (Eq) floats without treashold
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? == b.as_float()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::FloatNotEq(bin) => {
+                // Warning: comparing (!Eq) floats without treashold
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? != b.as_float()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::FloatLess(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? < b.as_float()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            Instruction::FloatLessOrEq(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? <= b.as_float()?;
+                    Ok(Data::from_int(res as i32))
+                })
+            },
+            // ---- ----
+            // Int arithmetics
+            Instruction::IntSum(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? + b.as_int()?;
+                    Ok(Data::from_int(res))
+                })
+            },
+            Instruction::IntDiff(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? - b.as_int()?;
+                    Ok(Data::from_int(res))
+                })
+            },
+            Instruction::IntMul(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? * b.as_int()?;
+                    Ok(Data::from_int(res))
+                })
+            },
+            Instruction::IntDiv(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? / b.as_int()?;
+                    Ok(Data::from_int(res))
+                })
+            },
+            Instruction::IntMod(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_int()? % b.as_int()?;
+                    Ok(Data::from_int(res))
+                })
+            },
+            // ---- ----
+            // Float arithmetics
+            Instruction::FloatSum(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? + b.as_float()?;
+                    Ok(Data::from_float(res))
+                })
+            },
+            Instruction::FloatDiff(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? - b.as_float()?;
+                    Ok(Data::from_float(res))
+                })
+            },
+            Instruction::FloatMul(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? * b.as_float()?;
+                    Ok(Data::from_float(res))
+                })
+            },
+            Instruction::FloatDiv(bin) => {
+                Self::binary_operator(&mut self.data_stack, &bin, |a, b|{
+                    let res = a.as_float()? / b.as_float()?;
+                    Ok(Data::from_float(res))
+                })
+            },
+            // ---- ----
+            // Pointers
+            Instruction::PointerGet(get) => {
+                let pointer = self.data_stack[&get.pointer].as_int()?;
+                let pointer_value = self.data_stack.global_pointer(pointer)?.clone();
+                self.data_stack[&get.copy_to] = pointer_value;
+                Ok(None)
+            },
+            Instruction::PointerSet(set) => {
+                let value = self.data_stack[&set.copy_from].clone();
+                let pointer = self.data_stack[&set.pointer].as_int()?;
+                let cell = self.data_stack.global_pointer_mut(pointer)?;
+                *cell = value;
+                Ok(None)
+            },
+            // ---- ----
+            // Function Call
+            Instruction::StackPointerAdd(offset) => {
+                self.data_stack.stack_pointer_add(*offset);
+                Ok(None)
+            },
+            Instruction::Call => {
+                let function_id = self.data_stack[&LocalDataPointer(0)].as_int()?;
+                let function_addr = self.functions[function_id as usize];
+                self.call_stack.push(self.instruction_pointer);
+                // Due to the fact that instruction pointer was incremented in the beginning
+                // of function saved value - pointer to instruction to be executed after 
+                // called function returned
+                self.instruction_pointer = function_addr;
+                Ok(None)
+            },
+            Instruction::Return => {
+                match self.call_stack.pop(){
+                    // Get pointer to instruction to be executed in caller
+                    // and jump there
+                    Some(instruction_pointer) => {
+                        self.instruction_pointer = instruction_pointer;
+                        Ok(None)
+                    },
+                    // If no pointer found (already in main function)
+                    // then terminate vm execution by returning value
+                    None => {
+                        let value = self.data_stack[&LocalDataPointer(0)].clone();
+                        Ok(Some(value))
+                    }
+                }
+            },
+            // ---- ----
+            // Constants
+            Instruction::IntConst(put_into, value) => {
+                self.data_stack[put_into] = Data::from_int(*value);
+                Ok(None)
+            },
+            Instruction::FloatConst(put_into, value) => {
+                self.data_stack[put_into] = Data::from_float(*value);
+                Ok(None)
+            },
+            // ---- ----
+            // Type convertions
+            Instruction::IntToFloat(conv) => {
+                let value = self.data_stack[&conv.source].as_int()?;
+                let value = value as f32;
+                self.data_stack[&conv.destination] = Data::from_float(value);
+                Ok(None)
+            },
+            Instruction::FloatToInt(conv) => {
+                let value = self.data_stack[&conv.source].as_float()?;
+                let value = value as i32;
+                self.data_stack[&conv.destination] = Data::from_int(value);
+                Ok(None)
+            },
+            // ---- ----
+            // Other
+            Instruction::Inc(pointer) => {
+                let value = self.data_stack[pointer].as_int()?;
+                self.data_stack[pointer] = Data::from_int(value + 1);
+                Ok(None)
+            },
+            Instruction::Dec(pointer) => {
+                let value = self.data_stack[pointer].as_int()?;
+                self.data_stack[pointer] = Data::from_int(value - 1);
+                Ok(None)
+            },
+            Instruction::Copy(copy) => {
+                let value = self.data_stack[&copy.source].clone();
+                self.data_stack[&copy.destination] = value;
+                Ok(None)
+            }
+
+            Instruction::Nop => {
+                Ok(None)
+            },
+        }
+    }
+
+    fn binary_operator<F>(data_stack: &mut DataStack<Data>, bin: &Binary, operator: F)->Result<Option<Data>, Data::Error>
+        where F: Fn(&Data, &Data)->Result<Data, Data::Error>
+    {
+        let res = {
+            let left = &data_stack[&bin.left];
+            let right = &data_stack[&bin.right];
+
+            operator(left, right)
+        };
+        data_stack[&bin.put_into] = res?;
+        Ok(None)
+    }
+}
 
 
 #[cfg(test)]
--- crates/vm/src/types.rs
+++ crates/vm/src/types.rs
@@ -1,5 +1,8 @@
 //! VirtualMachine supported types
 
+use std::fmt::Debug;
+use std::ops::{Index, IndexMut};
+
 
 /// Trait for data structure represention arbitrary data
 /// used in virtual machine
@@ -17,23 +20,34 @@
 /// where
 /// + $type_name - name for type in C (eg int, float)
 /// + $type - type used in rust (eg i32, f32)
-pub trait ArbitraryData{
+pub trait ArbitraryData: Debug + Clone + Default{
     /// type of error returned then trying access value 
     /// with sertain type and failing
-    type Error;
+    type Error: Debug;
 
     fn as_int(&self)->Result<i32, Self::Error>;
     fn from_int(value: i32)->Self;
 
     fn as_float(&self)->Result<f32, Self::Error>;
     fn from_float(value: f32)->Self;
+
+    fn access_error()->Self::Error;
 }
 
 
 /// Type safe (checking) implementation of ```ArbitraryData```
+#[derive(Clone, Debug)]
 pub enum ArbitraryDataEnum{
     Int(i32),
-    Float(f32)
+    Float(f32),
+    Void,
+}
+
+
+impl Default for ArbitraryDataEnum{
+    fn default()->Self{
+        ArbitraryDataEnum::Void
+    }
 }
 
 
@@ -59,5 +73,94 @@ impl ArbitraryData for ArbitraryDataEnum{
     }
     fn from_float(value: f32)->Self{
         ArbitraryDataEnum::Float(value)
+    }
+
+    fn access_error()->Self::Error{
+        ()
+    }
+}
+
+
+/// references local variable or temporary value
+#[derive(Copy, Clone, Debug)]
+pub struct LocalDataPointer(pub usize);
+
+
+/// Stack allocated data storage
+pub struct DataStack<Data>{
+    /// data
+    stack: Vec<Data>,
+    /// stack offset
+    stack_pointer: LocalDataPointer,
+
+    /// void value, reference to which returned in case of 
+    /// out of bound indexing (to avoid panicing)
+    void: Data,
+}
+
+
+impl<Data: ArbitraryData> Index<&LocalDataPointer> for DataStack<Data>{
+    type Output = Data;
+    /// interface to access data from stack
+    fn index(&self, pointer: &LocalDataPointer)->&Data{
+        let id = self.stack_pointer.0 + pointer.0;
+        if id < self.stack.len(){
+            &self.stack[id]
+        }else{
+            &self.void
+        }
+    }
+
+}
+
+impl<Data: ArbitraryData> IndexMut<&LocalDataPointer> for DataStack<Data>{
+    /// interface to mutate data in stack
+    ///
+    /// will automaticly reallocate if needed
+    fn index_mut<'a>(&mut self, pointer: &LocalDataPointer)->&mut Data{
+        let id = self.stack_pointer.0 + pointer.0;
+        if id >= self.stack.len(){
+            const OVERHEAD: usize = 10;
+            self.stack.resize_default(id + OVERHEAD);
+        }
+        &mut self.stack[id]
+    }
+}
+
+
+impl<Data: ArbitraryData> DataStack<Data>{
+    /// crete new instance of DataStack
+    pub fn new()->Self{
+        Self{
+            stack: vec![],
+            stack_pointer: LocalDataPointer(0),
+            void: Data::default(),
+        }
+    }
+
+    pub fn stack_pointer_add(&mut self, value: i32){
+        if value < 0{
+            self.stack_pointer.0 -= (-value) as usize;
+        }else{
+            self.stack_pointer.0 += value as usize;
+        }
+    }
+
+    pub fn global_pointer(&self, pointer: i32)->Result<&Data, Data::Error>{
+        let pointer = pointer as usize;
+        if pointer < self.stack.len(){
+            Ok(&self.stack[pointer as usize])
+        }else{
+            Err(Data::access_error())
+        }
+    }
+
+    pub fn global_pointer_mut(&mut self, pointer: i32)->Result<&mut Data, Data::Error>{
+        let pointer = pointer as usize;
+        if pointer < self.stack.len(){
+            Ok(&mut self.stack[pointer as usize])
+        }else{
+            Err(Data::access_error())
+        }
     }
 }
